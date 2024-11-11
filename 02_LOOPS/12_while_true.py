ed4867e914a39927c7f3d9068a100f8c201019b8
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2024-11-11 22:26:02 +0530

    Update 12_while_true.py

--- 02_LOOPS/12_while_true.py
+++ 02_LOOPS/12_while_true.py
@@ -3,7 +3,7 @@ while True:
     num2=int(input("enter a 2nd digit"))
     print(num1+num2)
     repeat=input("do you want to stop the program")
-    if repeat=="yes":
+    if repeat=="yess":
         break
 
 
