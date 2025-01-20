74463f65b7f37bb297ad479d9518847c9f218362
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2025-01-20 18:16:24 +0530

    advanced python

---
+++ 13_ADVANCED_PYTHON/02_ESCAPE_SEQUENCE.py
@@ -0,0 +1,19 @@
+print("hello \" bhushan\" world")
+print('hello \'hello \' world')
+
+# \n for new line 
+print("line A \nline B\nline C")
+
+# \t for TAb
+print("name \tBhushan")
+
+#\b for backspace after thi sone one element is eliminate
+print('hell\blo')
+
+#\ backslash 
+#print('this is backslash \')
+#it will through error because compiler is confused after \
+#To print backslah we have to use double backslash
+print("this is backslash \\")
+#to print double backslash 
+print("this is backslash \\\\")
