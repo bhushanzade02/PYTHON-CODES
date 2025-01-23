e8d737d81bd57c073475d6d2316a03317f4550b3
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2025-01-23 09:01:06 +0530

    23-01-25 practice

---
+++ 13_ADVANCED_PYTHON/26_CENTER_METHOD.py
@@ -0,0 +1,12 @@
+# name = "bhushan"
+# print(name.center(9, "*"))
+
+
+# name = input("Enter Your Name")
+# print(name.center(len(name)+8,"*"))
+
+
+
+
+name = input("enter a your name ")
+print(name.center(len(name)+8,"*"))
