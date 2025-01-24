b62f1dfd83a32ac44a6810044c56c9a8f9d4fa90
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2025-01-24 20:47:40 +0530

    24-01-25

---
+++ 13_ADVANCED_PYTHON/29_IF_ELSE.py
@@ -0,0 +1,16 @@
+#syntax 
+# if condition :
+       #code # if the condition is true this will execute
+
+
+age = input("enter your age")
+age = int(age)
+if age >= 14:
+    print("you are above 14")
+
+
+
+num = input('enter a number')
+num = int(num)
+if num >= 0:
+    print("Number is positive")
