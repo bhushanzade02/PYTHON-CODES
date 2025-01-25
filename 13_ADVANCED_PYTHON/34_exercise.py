0afa70be773869596ca18bc34c7f02746a78c39d
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2025-01-25 19:19:58 +0530

    update

---
+++ 13_ADVANCED_PYTHON/34_exercise.py
@@ -0,0 +1,7 @@
+name = input("enter your name ")
+age=10
+if age>=10 and (name[0]=="a" or name[0]=="A"):
+    print("you were eligible ")
+else:
+    print("you are not ")
+    eydgwudhfh3hfh3ifhifhihf
