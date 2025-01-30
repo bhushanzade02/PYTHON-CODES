eb087ac1eebb203f5092fd29bee5a50ab0bb1e5e
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2025-01-30 17:52:36 +0530

    30-1-25 Daily practice

---
+++ 13_ADVANCED_PYTHON/012_ASSIGNMNETb.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+float CtoF(float c)
+{
+    return (1.8 * c) + 32;
+}
+
+int main()
+{
+    float c;
+    float fahrenheit;
+
+    cout << "Enter the temperature in Celsius: " << endl;
+    cin >> c;
+
+    fahrenheit = CtoF(c);
+
+    cout << "Temperature in Fahrenheit: " << fahrenheit << endl;
+
+    return 0;
+}
+
+
+// OUTPUT:-
+// Enter the temperature in Celsius: 25
+// Temperature in Fahrenheit: 77
---
+++ 13_ADVANCED_PYTHON/73_LOOPING_WITH_LIST.py
@@ -0,0 +1,18 @@
+fruits=['apple','banana','kiwi','orange','pear']
+
+
+
+
+# for loop 
+for i in fruits:
+    print(i)
+
+
+
+
+
+# while loop 
+i=0
+while i < len(fruits):
+    print(fruits[i])
+    i+=1
