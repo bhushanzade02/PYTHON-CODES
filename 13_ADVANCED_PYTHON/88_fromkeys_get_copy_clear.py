a2f68a0626c3bb090faa133d8f6839686bb24f77
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2025-02-02 18:52:00 +0530

    update

---
+++ 13_ADVANCED_PYTHON/88_fromkeys_get_copy_clear.py
@@ -0,0 +1,46 @@
+#fromkeys
+# d = dict.fromkeys(['name', 'age', 'height'], 'unknown')
+# print(d)
+
+
+# d = dict.fromkeys('abc', 'unkonown')
+# print(d)
+
+
+# d = dict.fromkeys(range(1, 11), 'unknown')
+# print(d)
+
+
+
+# d = dict.fromkeys(['name', 'age'], ['unknown', 'unknown'])
+
+# print(d)
+
+
+d = {'name': 'bhushan', 'age': 'Unknown'}
+#  
+
+
+
+
+# if d.get('name'):
+#     print('present')
+# else:
+#     print('not present')
+
+
+
+
+
+## clear method
+
+# d.clear()
+# print(d)
+
+
+# to make copy of dictionary
+
+
+s=d.copy()
+print(d)
+print(s)
