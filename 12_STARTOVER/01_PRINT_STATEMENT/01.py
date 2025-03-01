177fe18a324da0d2c4d7d69bd732b0620f8c7dcb
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2025-03-01 18:16:16 +0530

    update

---
+++ 12_STARTOVER/01_PRINT_STATEMENT/01.py
@@ -0,0 +1,46 @@
+print('hello world')
+print('name:', "bhushan", 'age:', 24)
+print('Banana', 'Apple', 'Mango', sep=' , ')
+print('Python', 'IS', 'Awesome', sep='-')
+print('Hello', end=' ')
+print('World !')
+print("Processing..", end=" ")
+print("Done !")
+print('hello \nworld')
+print('hello \tworld')
+print("he said ,\"Pyhton is Fun!\"")
+name = "BUShan"
+age = 21
+print('MY name is ', name, "am i anm", age, "year old")
+print(f"my name is {name} and i am {age} year old")
+a, b = 10, 20
+print(f"the sum of {a}and {b} is {a+b}")
+
+with open('.txt', 'w') as file:
+    print("this is written to a file", file=file)
+    
+#PRACTICE QUESTION 
+print("WELCOME TO PROGRAMMING")
+print('name:', "BHSuhan", '\nage:', 21)
+print("21", '12', '222', sep=",")
+print('helo \nWorld ')
+print("\"PYhton is fun \"")
+print("Hello \tWorld ")
+
+a, b = 10, 20
+print(f"the sum of {a}and {b} is {a+b}")
+print("hello world ", end=' ')
+print('World')
+name = "alice"
+print(f"NAMe is {name}")
+
+with open(".txt", 'w') as file:
+    print("hello world", file=file)
+
+c= int(input("ENter a 1st num"))
+d = int(input("enter a 2nd number"))
+print(f"sum is {c+d}")
+rows = 5
+for i in range(1, rows):
+    print("*"*i)
+
