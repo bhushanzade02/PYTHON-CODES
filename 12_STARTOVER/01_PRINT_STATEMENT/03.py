a96fddb517a5a1f90be01ba55621020a02155fcf
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2025-03-11 20:35:14 +0530

    'UPDATE'

---
+++ 12_STARTOVER/01_PRINT_STATEMENT/03.py
@@ -0,0 +1,21 @@
+print("hello wrold")
+print(3.14)
+print("Name: ", "Bhsuhan", "AGe:", 21)
+print("aplle",'Banana','mangor',sep=",")
+print("aplle", 'Banana', 'mangor', sep="-")
+print("pyhton", "is", "awesome", sep="_")
+print("hello" ,end=" ")
+print("world")
+print("processing food", end=" ")
+print("done")
+print('i\'m bhushan')
+#if you want to print backlas
+print("\\")
+print("issc \n cms")
+print("issc \t cms")
+print("hello \b world ")
+print(r"line a \\an line b")
+
+name="Bhushan"
+age=21
+print(f"mY nme is {name} and my age is {age}")
