65f10ff95c0e5ce5245343848af52e74380b4a7a
bhushanzade02 <bhushanzade02@gmail.com>
2025-04-22 18:29:21 +0530

    Update

--- 12_STARTOVER/05_CONDITIONAL_STATEMENT/03.py
+++ 12_STARTOVER/05_CONDITIONAL_STATEMENT/03.py
@@ -23,4 +23,22 @@
 #     print("Grade D" )
 
 
+# name=input("Enter Your name")
+# if name:
+#     print(f"Your name is {name}")
+# else:
+#     print("you didn't type anything")
 
+
+
+# age =int(input("Enter Your age"))
+# citizen = input("Enter Country name")
+
+
+# if age >=18:
+#     if citizen=="india":
+#         print("Eligible to Vote in India")
+    
+marks=int(input("Enter your marks")) 
+    
+print("You will go to trip")  if marks>=90 else print("no mobile phone")    
---
+++ 12_STARTOVER/05_CONDITIONAL_STATEMENT/04_.py
@@ -0,0 +1,58 @@
+# 1. Even or Odd
+# Take a number as input. Print "Even" if it is divisible by 2, else print "Odd".
+
+
+
+# num=int(input("ENter a NUmber"))
+# if num%2==0:
+#     print("Even")
+# else:
+#     print("Odd")
+    
+    
+    
+# 2. Positive, Negative, or Zero
+# Take a number as input and print whether it is positive, negative, or zero.
+
+
+# num=int(input("ENter a number"))
+# if num >0:
+#     print("Positive")
+# elif num<0 :
+#     print("Negative")
+# else:
+#     print("Zero")
+
+
+
+
+# 3. Age Checker
+# Take age as input and:
+
+# Print “Child” if age < 13
+
+# Print “Teen” if age is between 13–19
+
+# Print “Adult” otherwise
+    
+    
+# age =int(input("Enter Your age "))
+
+# if age <13:
+#     print("child")
+# elif 13>= age <=19 :
+#     print("Teen")
+# else:
+#     print("Adult")
+
+
+
+# 11. Number Classification
+# Take a number and:
+
+# Check if it’s positive/negative
+
+# Also check if it’s even/odd
+
+number =int(input("Enter a NUmber"))
+if()
