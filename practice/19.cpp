9eab5bc76f436d4368e352ea77f1ce85d70f6881
Bhushan Zade <bhushanzade02@gmail.com>
2025-09-02 11:37:57 +0530

    Practice

---
+++ practice/19.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+// int main()
+// {
+//     int n = 5;
+//     for (int i = 0; i <=n; i++)
+//     {
+
+//         for (int j = 0; j < i; j++)
+//         {
+//             cout << "*";
+//         }
+
+//         cout << endl;
+//     }
+// }
+
+// int main()
+// {
+//     int n = 5;
+
+//     for (int i = 5; i > 0; i--)
+//     {
+//         for (int j = 0; j < n; j++)
+//         {
+//             cout << "*";
+//         }
+//         n--;
+//         cout << endl;
+//     }
+// }
---
+++ practice/20.py
@@ -0,0 +1,7 @@
+n = 5
+for i in range(n):
+    print(" " * i ,end ="")  #add leading spaces
+    for j in range(i,5):
+        print("*",end=" ")
+    print()
+   
