2bdfba3ecc64a316cb5448e3771cf9d79cfd7f10
Bhushan Zade <bhushanzade02@gmail.com>
2025-09-09 08:53:51 +0530

    practice

---
+++ practice/22.py
@@ -0,0 +1,25 @@
+# def prime_no(n):
+#     if n == 0 or n == 2 :
+#         return n
+#     else :
+#         result = []
+#         for i in range(3,n):
+#             if n% i == 0 :
+#                 result.append(i)
+#         return result
+
+
+# print(prime_no(10))
+
+# def isprime():
+#     n = int(input("enter the number"))
+#     if n <=1 :
+#         print("not a prime number")
+#     elif n ==2 :
+#         print("it is prime number")
+#     elif :
+#     for _ in range(2,n):
+#         if n%i ==0 :
+#             print("it is a prime num")
+    
+# print(isprime())
