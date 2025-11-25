862414a962cb52856f0e73618c88f556a90b7a28
BHUSHAN SANJAY ZADE <bhushanzade02@gmail.com>
2025-11-25 22:03:14 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,277 @@
+# a = [1,2,3]
+# b = a 
+# b.append(4)
+# print(a)
+
+
+# x = (1,2,3)
+
+# x[0] = 10
+
+
+# print("hello \n i am bhuushan \n thank you ")
+
+
+# print("""hello 
+#       i am bhushan 
+#       thank you """)
+
+
+# exp1 = eval(input("enter"))
+# print(exp1)
+
+
+
+# def function_creator():
+
+
+#     expr = input("enter the function for x")
+
+#     x= int(input("enter the value of x"))
+
+#     y = eval(expr)
+
+
+#     print(f"y :{y}")
+
+
+# if __name__ == "__main__":
+#     function_creator()
+
+
+
+# chars = ('a','b','c')
+# print(eval("'d' in chars"))
+# print(eval("'a' in chars"))
+
+
+# a = 7 
+# print(type(a))
+
+
+# b = 8
+# print(type(b))
+
+
+# c = a + b 
+
+# print(type(c))
+
+
+# d = 3 * 6.1
+# print(type(d))
+
+
+# c = 8.3
+
+# print(type(int(c)))
+# print((int(c)))
+# print(c)
+
+# c = 2
+
+# d = float(c)
+# print(d)
+# print(type(d))
+
+# e = str(c)
+# print(e)
+# print(type(e))
+
+
+
+# v = "string"
+
+# f = int(v)
+
+
+# print(type(f))
+
+
+# a = "3"
+
+# b = 5
+
+# c = int(a)+b
+# print(c)
+
+# a = 10
+# b = a
+# b <<=a
+
+# print(b)
+
+
+# a = 100 
+# b = 20
+
+# min = a if a  < b else b
+
+# print(min)
+
+# a = 10 
+# b = 29
+
+
+# print(a is not b)
+# print(a is b)
+# b = 10
+# for i in range(1,11):
+#     print(f"{i} == bin({bin(i)})")
+
+# print(bin(7))
+
+
+# a = "bhusidzade"
+
+
+# # print("sid" in a)
+# print("issc" not in a)
+
+
+# nested if statement 
+
+
+# marks = int(input("enter your marks"))
+
+
+# if marks >= 80 :
+#     print("you get goo dmark s")
+#     if(marks > 90 ):
+#         print("you can go to trip")
+# else :
+#     print("no phone")
+
+marks = 8
+# if marks >= 90 : print("you get good marks")
+
+
+# print("you get a good marks") if marks >= 90 else print("you have to study hard")
+# a = 2
+# n = 10 
+# for i in range(1,n+1):
+#     print(f"{a} x {i} = {i*a}")
+
+
+
+# i = 0 
+
+
+# while(i <  5):
+#     print(i)
+
+
+
+# n = "bhuszade"
+
+
+# # for i in n :
+# #     if i == "s":
+# #         continue
+# #     elif i == "z":
+# #         break
+# #     print(i)
+
+
+# for i in enumerate(n):
+#     print(i)
+
+# a = "why fit in , when you are born to standout "
+
+# print(len(a))
+# print(a.count("o"))
+# print(a.upper())
+# print(a.lower())
+# print(a.title())
+# print(a.find("in"))
+
+
+# a = "haryy Potter"
+# print(a.endswith("t",6,9))
+
+
+# print(a.swapcase())
+
+
+# a = ("****harry potter......")
+
+# print(a.strip("*,._"))
+# print(a.split("h"))
+# print(a.replace("harry","lorry"))
+
+# a = "harry potter and prisoner of azkaben"
+# print(a.rindex("a",6,20))
+# #print(a.rindex("prisoner"))
+
+
+
+a = "harry potter and goblet of the fire"
+# print(a[0:4])
+# # print(a[6:12])
+# print(a[:4])
+# print(a[-4:])
+# print(a[:-4])
+# print(a[::2])
+# print(a[::-1])
+
+
+# print(sorted(a))
+# print(a)
+
+# b = (a.replace("harry ","lorry"))
+# print(b)
+# print(a)
+
+
+
+
+# x = 10 
+# print(id(x))
+# x =20 
+# print(id(x))
+
+
+
+# x = [1,2,3]
+# print(id(x))
+
+# x.append("hello")
+# print(id(x))
+
+
+
+
+# name = "harry"
+
+# print(id(name))
+# name = name + "Potter"
+
+
+
+
+# print(id(name))
+
+
+# A = 10 
+# b = 10 
+
+
+# print(id(A))
+# print(id(b))
+
+
+
+# a = 1
+
+# b  = 1
+
+# print(id(a),id(b))
+
+# a = [1,2,3]
+# b = a
+# print(a,b)
+
+# del a 
+# del b 
+
+# print(a,b)
